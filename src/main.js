60a81e54cc7f42fbcc88b038d29c64c0796a4331
kimkyeongs <ksk@in-soft.co.kr>
2022-11-28 09:49:48 +0900

    hichart 설정

--- src/main.js
+++ src/main.js
@@ -1,19 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+import HighchartsVue from "highcharts-vue";
 
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
-import '@/assets/css/vuetify-grid.css'
-import '@/assets/css/style.css'
+import "@/assets/css/vuetify-grid.css";
+import "@/assets/css/style.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(HighchartsVue);
 
 new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    vuetify,
+    render: (h) => h(App),
+}).$mount("#app");
