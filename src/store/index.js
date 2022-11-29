0d5c25ac1f4950fa15093110f75c89abc058d6a1
kimkyeongs <ksk@in-soft.co.kr>
2022-11-29 13:45:11 +0900

    store 수정(jwt)

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import Vuex from "vuex";
 import authUser from "./modules/authUser";
 import settings from "./modules/setting";
 import getters from "./getters";
-//import createPersistedState from "vuex-persistedstate";
+import createPersistedState from "vuex-persistedstate";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
@@ -11,5 +11,12 @@ export default new Vuex.Store({
         authUser: authUser,
         settings: settings,
     },
+    plugins: [
+        //주목! : 여기에 쓴 모듈만 저장됩니다.
+        createPersistedState({
+            //storage: window.sessionStorage,
+            paths: ["settings", "authUser"],
+        }),
+    ],
     getters,
 });
